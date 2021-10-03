227a6393cbf1ffaf095c62893d03f228518c6839
jongracias <jonpgracias@gmail.com>
2021-10-03 10:15:22 -0400

    Final commit

--- app/src/main/java/com/bb/candylandcardgenerator/MainActivity.java
+++ app/src/main/java/com/bb/candylandcardgenerator/MainActivity.java
@@ -2,15 +2,14 @@ package com.bb.candylandcardgenerator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends Activity {
-    Integer[] candy = {R.drawable.candy_1, R.drawable.candy_2, R.drawable.candy_3, R.drawable.candy_4,
-            R.drawable.candy_5, R.drawable.candy_6};
+    ArrayList<Integer> candy = new ArrayList<>(4);
 
     Integer[] doubles = {R.drawable.double_1, R.drawable.double_2, R.drawable.double_3, R.drawable.double_4,
             R.drawable.double_5, R.drawable.double_6};
@@ -19,24 +18,36 @@ public class MainActivity extends Activity {
             R.drawable.single_5, R.drawable.single_6};
 
     final Random card = new Random();
-    final double deck = Math.random();
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        candy.add(R.drawable.candy_1);
+        candy.add(R.drawable.candy_2);
+        candy.add(R.drawable.candy_3);
+        candy.add(R.drawable.candy_4);
 
-        Button button = (Button) findViewById(R.id.button);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+        ImageView imageView = findViewById(R.id.imageView);
+        ImageView imageView2 = findViewById(R.id.imageView2);
 
-        button.setOnClickListener(v -> cardSelector(imageView));
+        button.setOnClickListener(v -> cardSelector(imageView, imageView2));
     }
 
-    void cardSelector(ImageView imageView) {
-        if(deck < .2) {
-            imageView.setImageResource(candy[card.nextInt(candy.length)]);
-        }else if(deck > .7){
+    void cardSelector(ImageView imageView, ImageView imageView2) {
+        double deck = Math.random();
+        imageView2.setImageDrawable(imageView.getDrawable());
+        int listIndex;
+        if(deck < .08 && !candy.isEmpty()) {
+            listIndex = card.nextInt(candy.size());
+            imageView.setImageResource(candy.get(listIndex));
+            candy.remove(listIndex);
+
+        }else if(deck > .85){
             imageView.setImageResource(doubles[card.nextInt(doubles.length)]);
+
         }else{
             imageView.setImageResource(singles[card.nextInt(singles.length)]);
         }
